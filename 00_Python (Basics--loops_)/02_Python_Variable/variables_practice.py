4493b9298f0d3fbed624e40e6181038d553e5d89
Adarsh Gawande <adarshgawande35@gmail.com>
2026-09-08 23:45:09 +0530

    Add files via upload

---
+++ 00_Python (Basics--loops_)/02_Python_Variable/variables_practice.py
@@ -0,0 +1,13 @@
+
+name = "Adarsh "
+age = 22
+city = "Amravati"
+marks = 75.5
+
+print("Name:", name)
+print("Age:", age)
+print("City:", city)
+print("Marks:", marks)
+
+marks = 82.5
+print("Updated Marks:", marks)
